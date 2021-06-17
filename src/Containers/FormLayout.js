47c6e56178e72ac88dbb509fbc2bab5b6fc7bb30
MuhammadIbrahimAlvi1 <ibrahimalvi122@gmail.com>
2021-06-18 01:17:47 +0500

    Error fixeed

--- src/Containers/FormLayout.js
+++ src/Containers/FormLayout.js
@@ -5,7 +5,6 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import Icon from "../assets/Icon.png";
-import { styles } from "./FormLayout.style";
 import CustomInput from "../Components/Inputs/CustomInput";
 import { useFormik } from "formik";
 import CustomRadio from "./../Components/Radios/CustomRadios";
